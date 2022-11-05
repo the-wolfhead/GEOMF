647a29558731d044afc861fdf7224ccb8954c64b
Daniel Otabor <danieldamianotabor@gmail.com>
2022-11-05 02:58:15 +0100

    creting template pages and setting up routes

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/donate', (req, res, next) => {
+  res.render('donate')
+})
+
+router.get('/calendar', (req, res, next) => {
+  res.render('calendar')
+})
+
+router.get('/gallery', (req, res, next) => {
+  res.render('gallery')
+})
+
 module.exports = router;
